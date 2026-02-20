8570238d6547917ba26e3dd79526df88a92e8042
debourouba <debourouba@gmail.com>
2026-02-20 19:13:24 +0000

    [dev] Backup: 2026-02-20 14:13

--- save.sh
+++ save.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 CONTAINER=n8n-dev
+echo "🔧 Préparation permissions..."
+sudo chown -R 1000:1000 ~/n8n-projects/workflows ~/n8n-projects/credentials
 echo "📤 Export workflows..."
 docker exec -u node $CONTAINER n8n export:workflow --backup --output=/home/node/n8n-projects/workflows
 echo "📤 Export credentials..."
 docker exec -u node $CONTAINER n8n export:credentials --backup --output=/home/node/n8n-projects/credentials
 sudo chown -R ubuntu:ubuntu ~/n8n-projects/workflows ~/n8n-projects/credentials
 echo "📦 Sync GitHub..."
-git -C ~/n8n-projects add .
 git -C ~/n8n-projects stash
 git -C ~/n8n-projects pull origin main --rebase
 git -C ~/n8n-projects stash pop
